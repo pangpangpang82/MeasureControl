caee38a1f6a86ba5a1b20991e8f680d0e057c7cc
取名叫做忍 <3306637503@qq.com>
2026-03-21 11:40:32 +0800

    Log修改前

--- MeasureControl/Models/ChannelCalibrationRecord.cs
+++ MeasureControl/Models/ChannelCalibrationRecord.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Prism.Mvvm;
 
 namespace MeasureControl.Models
@@ -41,6 +42,7 @@ namespace MeasureControl.Models
             }
         }
 
+        [JsonIgnore]
         public string DisplayChannelName
         {
             get
