6280a82e0fb17abe9ac08b57d51364dde929f560
取名叫做忍 <3306637503@qq.com>
2026-03-21 10:42:49 +0800

    交互信息调整前 终局之章 十三枪你没有心

--- MeasureControl/ViewModels/SingleBoardTest/HydraulicController/HC_6_5ViewModel.cs
+++ MeasureControl/ViewModels/SingleBoardTest/HydraulicController/HC_6_5ViewModel.cs
@@ -770,6 +770,7 @@ namespace MeasureControl.ViewModels.SingleBoardTest.HydraulicController
                     break;
             }
             RefreshMeasureCommand();
+            await TryFinalizeAsync().ConfigureAwait(false);
         }
 
         private async Task OnMeasureCustomCurrentAsync()
