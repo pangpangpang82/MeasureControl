7419e4a9e6a13c5fdf356a0f8a5025985078c13e
pangpangpang82 <15168@LENOVO>
2026-03-26 16:11:00 +0800

    电阻

--- MeasureControl/App.xaml.cs
+++ MeasureControl/App.xaml.cs
@@ -91,7 +91,6 @@ namespace MeasureControl
             containerRegistry.RegisterSingleton<SignalValueUpdateService>();
             containerRegistry.RegisterSingleton<ISingleBoardTestContextService, SingleBoardTestContextService>();
             containerRegistry.RegisterSingleton<IHydraulicPowerService, HydraulicPowerService>();
-            containerRegistry.RegisterSingleton<PxiChassis2AutoEnableService>();
             // 注册硬件API（可选，用于单板测试）
             containerRegistry.Register<IDmmApi, DmmSocketApi>();
             containerRegistry.RegisterSingleton<IComponentPowerStateApi, ComponentPowerStateApi>();
